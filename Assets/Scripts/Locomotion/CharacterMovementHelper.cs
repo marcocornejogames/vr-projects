885dabee7813dbafa34dc781ffa877d74dfc1c49
Marco <manto.cornejo@gmail.com>
2022-01-26 17:10:08 -0500

    Movement Physics Added

--- Assets/Scripts/Locomotion/CharacterMovementHelper.cs
+++ Assets/Scripts/Locomotion/CharacterMovementHelper.cs
@@ -4,42 +4,50 @@ using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+[RequireComponent(typeof(XROrigin))]
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterControllerDriver))]
 public class CharacterMovementHelper : MonoBehaviour
 {
     [Header("Component References")]
     [SerializeField] private XROrigin _XROrigin;
-    [SerializeField] private CharacterController _CharacterController;
-    [SerializeField] private CharacterControllerDriver _CharacterControllerDriver;
-    
-    
-    
-    // Start is called before the first frame update
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private CharacterControllerDriver _characterControllerDriver;
+
+    //UNITY MESSAGES _____________________________________________________________
+    private void Awake()
+    {
+        _XROrigin = GetComponent<XROrigin>();
+        _characterController = GetComponent<CharacterController>();
+        _characterControllerDriver = GetComponent<CharacterControllerDriver>();
+    }
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         UpdateCharacterController();
     }
 
+
+    //CUSTOM METHODS ______________________________________________________________
+
     /// <summary>
     /// Updates the <see cref="CharacterController.height"/> and <see cref="CharacterController.center"/>
     /// based on the camera's position.
     /// </summary>
-    protected virtual void UpdateCharacterController()
+    protected virtual void UpdateCharacterController() //Taken from CharacterControllerDriver
     {
-        if (_XROrigin == null || _CharacterController == null)
+        if (_XROrigin == null || _characterController == null)
             return;
 
-        var height = Mathf.Clamp(_XROrigin.CameraInOriginSpaceHeight, _CharacterControllerDriver.minHeight, _CharacterControllerDriver.maxHeight);
+        var height = Mathf.Clamp(_XROrigin.CameraInOriginSpaceHeight, _characterControllerDriver.minHeight, _characterControllerDriver.maxHeight);
 
         Vector3 center = _XROrigin.CameraInOriginSpacePos;
-        center.y = height / 2f + _CharacterController.skinWidth;
+        center.y = height / 2f + _characterController.skinWidth;
 
-        _CharacterController.height = height;
-        _CharacterController.center = center;
+        _characterController.height = height;
+        _characterController.center = center;
     }
 }
